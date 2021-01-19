caf0ffaaddea51beb5bf1ebb2186223069809329
Raffaele Ariano <raffaele.aariano@gmail.com>
2021-01-19 21:33:34 +0100

    finished
    what a relief

---
+++ Mini Platform Game/KinematicBody2D.gd
@@ -0,0 +1,39 @@
+extends KinematicBody2D
+
+const UP=Vector2(0,-1) #la direzione su
+const GRAVITY=20
+const MAX_SPEED=200
+const ACCELLERATION=50
+const JUMP_HEIGHT=-550
+var motion=Vector2()
+
+func _physics_process(delta):
+	motion.y+=GRAVITY
+	var friction = false
+	if Input.is_action_pressed("ui_right"):
+		motion.x=min(motion.x+ACCELLERATION,MAX_SPEED)
+		$Sprite.flip_h=false
+		$Sprite.play("Run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x=max(motion.x-ACCELLERATION,-MAX_SPEED)
+		$Sprite.flip_h=true
+		$Sprite.play("Run")
+	else:
+		$Sprite.play("Idle")
+		friction=true
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y=JUMP_HEIGHT
+		if(friction):
+			motion.x=lerp(motion.x,0,0.2)
+	else:
+		if(motion.y<0):
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if(friction):
+			motion.x=lerp(motion.x,0,0.05)
+	
+	motion=move_and_slide(motion,UP)
+	pass
